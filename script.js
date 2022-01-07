8797937207999bab50da1df15d787e8ce41aa3d9
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-07 10:05:31 -0300

    addList

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-window.addEventListener('load', () => {
-    const tarefa = document.querySelector('#tarefa');
-    const input = document.querySelector('.input');
-    const wrapper = document.querySelector('.wrapper');
-})
+const input = document.querySelector('.input');
+const btn = document.querySelector(#add);
+
+function addList(e) {
+    const afazer = document.querySelector('.afazer');
+    const feito = document.querySelector('.feito');
+
+    const newLi = document.createElement('li');
+    const checkBtn = document.createElement('#checkbox');
+    const delBtn = document.createElement('#delete');
+    const editBtn = document.createElement('#edit');
+}
