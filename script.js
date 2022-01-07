52e4314b6efd1e9aaf4c11b5fab351c93a678947
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-06 22:41:38 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const tarefa = document.querySelector('#tarefa');
+    const input = document.querySelector('.input');
+    const wrapper = document.querySelector('.wrapper');
+})
