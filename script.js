4f103d158c519ff5d5f4ff263f1cc37e5e5f35b6
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-08 09:46:02 -0300

    inputval

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var count = 0;
+    $(".button").click(function() {
+        var inputval = $(".input").val();
+        if (inputval != '') {
+            count ++;
+            $(".afazer").append('<li class="bloco"><input id="checkbox-'+count+'" type="checkbox"><label for="checkbox-'+count+'">'+inputval+'</label><button id="delete"><i class="fa fa-trash"></i></button><button id="edit"><i class="fas fa-pen"></i></button></li> ')
+        }
+    })
+})
