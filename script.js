7dcc9c26461105e1c8edfc17b8e4f471336f794d
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-09 23:10:39 -0300

    finale

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
     var count = 0;
     $(".button").click(function() {
-        var inputval = $(".input").val();
+        var inputval = $(".input").val(); 
+        let webtask = localStorage.getItem("localtask");
         if (inputval != '') {
             count ++;
             $(".afazer").append('<li class="bloco"><input id="checkbox-'+count+'" type="checkbox"><label for="checkbox-'+count+'">'+inputval+'</label><button id="delete"><i class="fa fa-trash"></i></button><button id="edit"><i class="fas fa-pen"></i></button></li> ')
-        }
+        }else
          $(".input").val(''); 
+        if(webtask == null) {
+            taskObj = [];
+        }else {
+            taskObj = JSON.parse(webtask);
+        }
+        taskObj.push({'task_name':inputval, 'completeStatus':false});
+        console.log(taskObj, 'tarefa');
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        input.value = '';
     })
     const Feito = document.querySelector(".feito");
-    const edit = document.querySelector("#edit");
-    const bloco  = document.querySelector(".bloco");
     const input = document.querySelector(".input");
 
     $(document).on('click', '.feito #delete, .afazer #delete' , function() {
@@ -23,4 +31,6 @@ $(document).ready(function() {
         Feito.appendChild(parent);
         
     });
+
+    
 })
